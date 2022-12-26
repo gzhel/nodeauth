4f921bd7eaae692190193c9b8060abc733135118
gzhel <gregoryzhel@gmail.com>
2022-12-26 22:34:29 +0700

    feature/nodeauth-1 - nodejs authorization using jwt

---
+++ server/dtos/userDTO.js
@@ -0,0 +1,13 @@
+class UserDTO {
+    email;
+    id;
+    isActivated;
+
+    constructor(model) {
+        this.email = model.email;
+        this.id = model._id;
+        this.isActivated = model.isActivated;
+    }
+}
+
+module.exports = UserDTO;
---
+++ server/models/tokenModel.js
@@ -0,0 +1,8 @@
+const {Schema, model} = require('mongoose');
+
+const TokenSchema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'User'}, // link on user
+    refreshToken: {type: String, required: true},
+});
+
+module.exports = model('Token', TokenSchema);
